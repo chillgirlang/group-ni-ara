b39b23e6c7319dfbe295a477db2d315b184af0d0
[Lawrence Mindanao] <[mindanaolawrence86@gmail.com]>
2025-02-19 21:54:21 +0800

    Slight alteration of welcome page title

--- medical_case_study(laravelbackend)/resources/views/welcome.blade.php
+++ medical_case_study(laravelbackend)/resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Dental Medical Clinic</title>
+        <title>Medical Clinic</title>
     </head>
     <body>
         <header>
